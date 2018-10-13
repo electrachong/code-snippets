f9401ceac78a560b947967ccd5be7700fe6cee84
Electra Chong <electra@darbysmart.com>
2018-10-13 14:26:20 -0700

    add syntactic sugar and more fns

--- snippets/sort_iternary.js
+++ snippets/sort_iternary.js
@@ -10,13 +10,32 @@ Example inputs:
   [1,2], [2,3], [3,4]
 
 TODO:
-  - More syntactic sugar
+  - Probably don't need to push markers to array
 */
 
 const { printDebug } = require('../util/util');
 
 const START = Symbol('start');
 const END = Symbol('end');
+
+class Trip {
+  constructor(departure, destination) {
+    this._departure = departure;
+    this._destination = destination;
+  }
+
+  get departure() {
+    return this._departure;
+  }
+
+  get destination() {
+    return this._destination;
+  }
+
+  toArray() {
+    return [this._departure, this._destination];
+  }
+}
 
 function getTripMappings(unsortedList) {
   const destinationByDeparture = {};
@@ -34,35 +53,33 @@ function getTripMappings(unsortedList) {
 // Find the item where the previous departure was the destination
 function findPreviousTrip(trip, departureByDestination) {
   const [departure, destination] = trip;
-  return [departureByDestination[departure], departure];
+  return new Trip(departureByDestination[departure], departure);
 }
 
 // Find the item where the previous destination is the departure
 function findNextTrip(trip, destinationByDeparture) {
   const [departure, destination] = trip;
-  return [destination, destinationByDeparture[destination]];
+  return new Trip(destination, destinationByDeparture[destination]);
 }
 
-function sortIternary(unsortedList) {
-  const [destinationByDeparture, departureByDestination] = getTripMappings(unsortedList);
-  const sortedList = [];
-  sortedList.push(unsortedList[0]);
-
-  // Find the previous trip of the first item in the array, add to the array
-  // and repeat until we have encountered the first trip.
+// Find the previous trip of the first item in the array, add to the array
+// and repeat until we have encountered the first trip.
+function sortListToStart(sortedList, departureByDestination) {
   for (let firstItem = sortedList[0]; firstItem !== START; firstItem = sortedList[0]) {
     const previousTrip = findPreviousTrip(firstItem, departureByDestination);
     printDebug('previousTrip', previousTrip);
-    if (previousTrip[0] === undefined) {
+    if (previousTrip.departure === undefined) {
       // add start marker to indicate we've found the beginning
       sortedList.unshift(START);
     } else {
-      sortedList.unshift(previousTrip);
+      sortedList.unshift(previousTrip.toArray());
     }
   }
+}
 
-  // Find the next trip of the last item in the array, add to the array
-  // and repeat until we have encountered the last trip.
+// Find the next trip of the last item in the array, add to the array
+// and repeat until we have encountered the last trip.
+function sortListToEnd(sortedList, destinationByDeparture) {
   for (
     let lastItem = sortedList[sortedList.length - 1];
     lastItem !== END;
@@ -70,13 +87,22 @@ function sortIternary(unsortedList) {
   ) {
     const nextTrip = findNextTrip(lastItem, destinationByDeparture);
     printDebug('nextTrip', nextTrip);
-    if (nextTrip[1] === undefined) {
+    if (nextTrip.destination === undefined) {
       // add end marker to indicate we've found the end
       sortedList.push(END);
     } else {
-      sortedList.push(nextTrip);
+      sortedList.push(nextTrip.toArray());
     }
   }
+}
+
+function sortIternary(unsortedList) {
+  const [destinationByDeparture, departureByDestination] = getTripMappings(unsortedList);
+  const sortedList = [];
+  sortedList.push(unsortedList[0]);
+
+  sortListToStart(sortedList, departureByDestination);
+  sortListToEnd(sortedList, destinationByDeparture);
 
   return sortedList.slice(1, sortedList.length - 1);
 }
